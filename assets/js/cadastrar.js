0b354069a2d4d666e875e3096f71ce4c3c0b568d
gabmeloo <gabrielademelo.9c@gmail.com>
2023-10-11 22:35:07 -0300

    Adição dos Cards

--- assets/js/cadastrar.js
+++ assets/js/cadastrar.js
@@ -1,7 +1,25 @@
 // => = abreviação da função function onde se usa quando não se precisa de uma função para o projeto todo
 //ou quando vc só precisa de uma unica função
+// || = ou
 document.querySelector("#botao-cadastrar").addEventListener("click", () => {
-    const titulo = document.querySelector("#titulo").value
-    console.log(titulo)
+
+    
+    let tarefas = JSON.parse(localStorage.getItem("tarefas")) || []
+
+    console.log(tarefas)
+
+  
+    const tarefa = {
+        titulo: document.querySelector("#titulo").value,
+        descricao: document.querySelector("#descricao").value,
+        pontos: document.querySelector("#pontos").value
+    }
+    
+    tarefas.push(tarefa)
+
+    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+
+    window.location.href = "index.html"
+   
 })
 
